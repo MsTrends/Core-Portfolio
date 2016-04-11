bb59dc0e603175cb010d2e28e7607b99766c4994
Muhammad Faisal <themekrafts@gmail.com>
2016-04-12 02:51:22 +0500

    Updated prefixes from ms_ to th_

--- functions.php
+++ functions.php
@@ -2,11 +2,11 @@
 
 /*
 Plugin Name: Core Portfolio
-Plugin URI: http://mstrends.com/plugins/portfolio-post-type
+Plugin URI: https://github.com/themeshash/core-portfolio
 Description: Creates a Specific CPT + Taxonomy + Widget for Portfolio.
-Version: 1.1
+Version: 1.2
 Author: Muhamamd Faisal
-Author URI: http://themeforest.net/user/MsTrends
+Author URI: http://themeforest.net/user/ThemesHash
 */
 
 
@@ -32,15 +32,15 @@ Author URI: http://themeforest.net/user/MsTrends
 # Setup Core Plugin (fires on every page load)
 #-----------------------------------------------------------------# 
 
-    function ms_setup_portfolio() {
+    function th_setup_portfolio() {
 
         // initiate classes
-        new MS_Portfolio_CPT();
-        new MS_Portfolio_Taxonomies();
+        new TH_Portfolio_CPT();
+        new TH_Portfolio_Taxonomies();
 
     }
 
-    ms_setup_portfolio();
+    th_setup_portfolio();
 
 
 #-----------------------------------------------------------------#
@@ -48,24 +48,25 @@ Author URI: http://themeforest.net/user/MsTrends
 #-----------------------------------------------------------------# 
 
     // On Plugin Activation
-    function ms_portfolio_activate() { 
+    function th_portfolio_activate() { 
 
-        ms_setup_portfolio();
+        th_setup_portfolio();
+        th_portfolio_install();
         flush_rewrite_rules();
 
     }
 
-    register_activation_hook( __FILE__, 'ms_portfolio_activate' );
+    register_activation_hook( __FILE__, 'th_portfolio_activate' );
      
 
     // On Plugin Deactivation
-    function ms_portfolio_deactivate() {
+    function th_portfolio_deactivate() {
 
         flush_rewrite_rules();
 
     }
 
-    register_deactivation_hook( __FILE__, 'ms_portfolio_deactivate' );
+    register_deactivation_hook( __FILE__, 'th_portfolio_deactivate' );
 
 
 #-----------------------------------------------------------------#
@@ -73,7 +74,7 @@ Author URI: http://themeforest.net/user/MsTrends
 #-----------------------------------------------------------------# 
 
 
-    function ms_enqueue_portfolio_scripts() {
+    function th_enqueue_portfolio_scripts() {
         if ( is_admin() ) {
 
             // Register CSS
@@ -85,34 +86,62 @@ Author URI: http://themeforest.net/user/MsTrends
         }
     }
 
-    add_action( 'admin_enqueue_scripts', 'ms_enqueue_portfolio_scripts' );    
+    add_action( 'admin_enqueue_scripts', 'th_enqueue_portfolio_scripts' );    
 
 
 #-----------------------------------------------------------------#
-#  Resolves CPT vs Page Slug Issue
+#  Create New Page on Plugin Activation
 #-----------------------------------------------------------------# 
 
-    if ( !function_exists('portfolio_is_bad_hierarchical_slug') ) {
+    // if ( !function_exists('portfolio_is_bad_hierarchical_slug') ) {
 
-        function portfolio_is_bad_hierarchical_slug( $is_bad_hierarchical_slug, $slug, $post_type, $post_parent ) {
-            if ( !$post_parent && $slug == 'portfolio' )
-                return true;
-            return $is_bad_hierarchical_slug;
-        }
+    //     function portfolio_is_bad_hierarchical_slug( $is_bad_hierarchical_slug, $slug, $post_type, $post_parent ) {
+    //         if ( !$post_parent && $slug == 'portfolio' )
+    //             return true;
+    //         return $is_bad_hierarchical_slug;
+    //     }
 
+    // }
+
+    // add_filter( 'wp_unique_post_slug_is_bad_hierarchical_slug', 'portfolio_is_bad_hierarchical_slug', 10, 4 );
+
+
+    // if ( !function_exists('portfolio_is_bad_flat_slug') ) {
+
+    //     function portfolio_is_bad_flat_slug( $is_bad_flat_slug, $slug, $post_type ) {
+    //         if ( $slug == 'portfolio' )
+    //             return true;
+    //         return $is_bad_flat_slug;
+    //     }
+
+    // }
+
+    // add_filter( 'wp_unique_post_slug_is_bad_flat_slug', 'portfolio_is_bad_flat_slug', 10, 3 );
+
+
+    function th_portfolio_install() {
+        global $wp_version;
+
+        if( version_compare( $wp_version, '3.5', '<' ) ) {
+            wp_die( 'Detta tilläget kräver att du har WordPress version 3.5 eller högre.' );
+        } else {
+            if(!get_page_by_slug('portfolio')) {
+                $product_page = array(
+                    'post_type' => 'page',
+                    'post_name' => 'portfolio',
+                    'post_title' => 'Portfolio',
+                    'post_status' => 'publish',
+                );
+
+                wp_insert_post($product_page);
+            }
+        }   
     }
 
-    add_filter( 'wp_unique_post_slug_is_bad_hierarchical_slug', 'portfolio_is_bad_hierarchical_slug', 10, 4 );
 
-
-    if ( !function_exists('portfolio_is_bad_flat_slug') ) {
-
-        function portfolio_is_bad_flat_slug( $is_bad_flat_slug, $slug, $post_type ) {
-            if ( $slug == 'portfolio' )
-                return true;
-            return $is_bad_flat_slug;
-        }
-
-    }
-
-    add_filter( 'wp_unique_post_slug_is_bad_flat_slug', 'portfolio_is_bad_flat_slug', 10, 3 );
+    function get_page_by_slug($slug) {
+        if ($pages = get_pages())
+            foreach ($pages as $page)
+                if ($slug === $page->post_name) return $page;
+        return false;
+    } // function get_page_by_slug
